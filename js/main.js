e20af7c81b24b58349ccb3f9287f54c437e45efb
Teema <temuulenhover@gmail.com>
2021-09-27 14:40:15 +0800

    Chess

---
+++ js/main.js
@@ -0,0 +1,75 @@
+// const scrollContainer = document.querySelector("main");
+
+// scrollContainer.addEventListener("wheel", (evt) => {
+//     evt.preventDefault();
+//     scrollContainer.scrollLeft += evt.deltaY;
+// });
+
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+var modal1 = document.getElementById("myModal1");
+var btn1 = document.getElementById("myBtn1");
+var span1 = document.getElementsByClassName("close1")[0];
+
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+
+
+
+// btnLesson.onclick = function() {
+//   modalLesson.style.display = "block";
+// }
+
+var video = document.querySelector("#comingSoonVideo");
+var videoContainer = document.querySelector("#comingSoonVideoContainer");
+
+
+
+videoContainer.onmousemove = handleMouseMove;
+var videoPlayBackSpeed = 0.5;
+var videoPlayBackSpeedAmount = 0.01;
+video.playbackRate = videoPlayBackSpeed;
+
+function handleMouseMove(event) {
+    if (event.pageX < 500) {
+        if (videoPlayBackSpeed - videoPlayBackSpeedAmount >= 0.35) {
+            videoPlayBackSpeed -= videoPlayBackSpeedAmount;
+        }
+    }
+
+    if (event.pageX > 500) {
+        if (videoPlayBackSpeed + videoPlayBackSpeedAmount <= 1) {
+            videoPlayBackSpeed += videoPlayBackSpeedAmount;
+        }
+    }
+
+    video.playbackRate = videoPlayBackSpeed;
+}
---
+++ js/swiper.js
@@ -0,0 +1,150 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    mousewheel: true,
+    speed: 1000,
+
+});
+
+var nav = document.getElementById("nav");
+
+function slideTo(i) {
+    let mql = window.matchMedia('(max-width: 400px)');
+    if (mql.matches == true) {
+        nav.classList.remove("show");
+        nav.classList.toggle("hide");
+    }
+    swiper.slideTo(i, 1000)
+
+}
+const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    loop: true,
+    direction: 'horizontal',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 0,
+        slideShadows: false,
+    },
+});
+const swiperTeachers = new Swiper('.swiperTeachers', {
+    // Optional parameters
+    loop: true,
+    direction: 'horizontal',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+
+});
+const swiperTeachersContent = new Swiper('.swiperTeachersContent', {
+    // Optional parameters
+    loop: true,
+    direction: 'horizontal',
+
+    effect: 'flip',
+    flipEffect: {
+        slideShadows: false,
+    },
+
+});
+
+swiperTeachers.controller.control = swiperTeachersContent
+swiperTeachersContent.controller.control = swiperTeachers
+
+const swiperTraining = new Swiper('.swiperTraining', {
+    // Optional parameters
+    direction: 'vertical',
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
+
+function slideToTraining(i) {
+    swiperTraining.slideTo(i, 1000)
+}
+const swiperLessonRight1 = new Swiper('.swiperLessonRight1', {
+    direction: 'vertical',
+});
+const swiperLessonRight2 = new Swiper('.swiperLessonRight2', {
+    direction: 'vertical',
+});
+const swiperLessonRight3 = new Swiper('.swiperLessonRight3', {
+    direction: 'vertical',
+});
+const swiperLessonRight4 = new Swiper('.swiperLessonRight4', {
+    direction: 'vertical',
+});
+const swiperLessonRight5 = new Swiper('.swiperLessonRight5', {
+    direction: 'vertical',
+});
+const swiperLessonRight6 = new Swiper('.swiperLessonRight6', {
+    direction: 'vertical',
+});
+
+
+function slideToLesson1(i) {
+    swiperLessonRight1.slideTo(i, 1000)
+}
+
+function slideToLesson2(i) {
+    swiperLessonRight2.slideTo(i, 1000)
+}
+
+function slideToLesson3(i) {
+    swiperLessonRight3.slideTo(i, 1000)
+}
+
+function slideToLesson4(i) {
+    swiperLessonRight4.slideTo(i, 1000)
+
+}
+
+function slideToLesson5(i) {
+    swiperLessonRight5.slideTo(i, 1000)
+}
+
+function slideToLesson6(i) {
+    swiperLessonRight6.slideTo(i, 1000)
+}
+var burgerButton = document.getElementById("burgerBtn");
+var closeNav = document.getElementById("closeNav");
+var nav = document.getElementById("nav");
+burgerButton.onclick = myFunction1;
+closeNav.onclick = myFunction2;
+
+function myFunction1(e) {
+    e.preventDefault();
+    nav.classList.toggle("show");
+}
+
+function myFunction2(e) {
+    e.preventDefault();
+    nav.classList.remove("show");
+    nav.classList.toggle("hide");
+}
+
+
+function enterNavigationOnMobile(e) {
+    e.preventDefault();
+    nav.classList.toggle("show");
+}
+
+function exitNavigationOnMobile(e) {
+    e.preventDefault();
+    nav.classList.toggle("show");
+}
